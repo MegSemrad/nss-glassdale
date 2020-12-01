163331b2b6b931e3a666c33f538b9713b2ac7734
MegSemrad <mlsemrad753@gmail.com>
2020-12-01 15:09:02 -0600

    added css

--- scripts/criminals/criminals.js
+++ scripts/criminals/criminals.js
@@ -1,11 +1,11 @@
 export const Criminals = (criminals) => {
     return `
         <section class="criminals">
-            <div class="criminals__name">${criminals.name}</div>
-            <div class="criminals__age">${criminals.age}</div>
-            <div class="criminals__crime">${criminals.conviction}</div>
-            <div class="criminals__term__start">${new Date(criminals.incarceration.start).toLocaleDateString('en-US')}</div>
-            <div class="criminals__term__end">${new Date(criminals.incarceration.end).toLocaleDateString('en-US')}</div>
+            <h2 class="criminals__name">${criminals.name}</h2>
+            <div class="criminals__age criminals__data">${criminals.age}</div>
+            <div class="criminals__crime criminals__data">${criminals.conviction}</div>
+            <div class="criminals__term__start criminals__data">${new Date(criminals.incarceration.start).toLocaleDateString('en-US')}</div>
+            <div class="criminals__term__end criminals__data">${new Date(criminals.incarceration.end).toLocaleDateString('en-US')}</div>
         </section>
     `
 }
