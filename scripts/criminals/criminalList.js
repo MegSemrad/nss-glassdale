8fe48b16d452137b8c8ccfb1a1e9f0003ab750fc
MegSemrad <mlsemrad753@gmail.com>
2020-12-01 21:02:51 -0600

    added class notes

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -18,17 +18,3 @@ export const CriminalList = () => {
     )
 };
 
-/*
-- Passed a url to fetch as an argument and it returns an object
-- fetch is a function that returns an object!!
-- .then() is chained on 
-- that return object is a promise object - which has property on it called 'then'
-- .then() is a method on a promise object
-- getCriminals returns the results of calling fetch which is a promise object so getCriminals istelf 
-now returns a promise object
-- .then() means wait until getCriminals does its thing (ie the promise is resolved) THEN do the nect thing
-*/
-
-/*
-
-*/
