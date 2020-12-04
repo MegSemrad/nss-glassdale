69d983ac4357e00c34782c3c5b08c8f52dd79eea
MegSemrad <mlsemrad753@gmail.com>
2020-12-04 16:06:11 -0600

    worked on chp5

--- scripts/convictions/convictionSelect.js
+++ scripts/convictions/convictionSelect.js
@@ -1,21 +1,49 @@
-/*
- *   ConvictionSelect component that renders a select HTML element
- *   which lists all convictions in the Glassdale PD API
- */
 import { useConvictions, getConvictions } from "./ConvictionProvider.js"
 
-// Get a reference to the DOM element where the <select> will be rendered
+
 const contentTarget = document.querySelector(".filters__crime")
+const eventHub = document.querySelector(".container")
+
+
+
+
+
+// On the event hub, listen for a "change" event.
+eventHub.addEventListener("change", event => {
+
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.id === "crimeSelect") {
+        // Create custom event. Provide an appropriate name.
+        const selectedCrimeEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeThatWasChosen: event.target.value
+                /*the value is crime.id...look down in 
+                convictionsCollection function and 
+                the <option> has the value of ${crime.id} */
+            }
+        })
+
+        // Dispatch to event hub
+        eventHub.dispatchEvent(selectedCrimeEvent)
+    }
+})
+
+
+
+
+
 
 export const ConvictionSelect = () => {
     // Trigger fetching the API data and loading it into application state
     getConvictions()
     .then( () => {
-      // Get all convictions from application state
       const convictions = useConvictions()
       render(convictions)
     })
 }
+
+
+
 
 
 /*
@@ -29,11 +57,14 @@ const render = convictionsCollection => {
         <select class="dropdown" id="crimeSelect">
             <option value="0">Please select a crime...</option>
             ${
-                convictionsCollection.map(crimesObject => {
-                    const crimeName = crimesObject.name
-                    return `<option>${crimeName}</option>`
-                })
+                convictionsCollection.map((crime) => 
+                  `<option ${crime.id}>${crime.name}</option>`
+                )
             }
         </select>
     `
 }
+
+
+
+
--- scripts/criminals/criminals.js
+++ scripts/criminals/criminals.js
@@ -1,11 +1,11 @@
-export const Criminals = (criminals) => {
+export const Criminals = (criminalObject) => {
     return `
         <section class="criminals">
-            <h2 class="criminals__name">${criminals.name}</h2>
-            <div class="criminals__age criminals__data">${criminals.age}</div>
-            <div class="criminals__crime criminals__data">${criminals.conviction}</div>
-            <div class="criminals__term__start criminals__data">${new Date(criminals.incarceration.start).toLocaleDateString('en-US')}</div>
-            <div class="criminals__term__end criminals__data">${new Date(criminals.incarceration.end).toLocaleDateString('en-US')}</div>
+            <h2 class="criminals__name">${criminalObject.name}</h2>
+            <div class="criminals__age criminals__data">${criminalObject.age}</div>
+            <div class="criminals__crime criminals__data">${criminalObject.conviction}</div>
+            <div class="criminals__term__start criminals__data">${new Date(criminalObject.incarceration.start).toLocaleDateString('en-US')}</div>
+            <div class="criminals__term__end criminals__data">${new Date(criminalObject.incarceration.end).toLocaleDateString('en-US')}</div>
         </section>
     `
 }
