b46b2fcf88895ee2c36a9da4ca8c9b21733ad52d
MegSemrad <mlsemrad753@gmail.com>
2020-12-08 13:02:26 -0600

    added notes button

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,11 @@ import { CriminalList } from './criminals/criminalList.js';
 import { ConvictionSelect } from './convictions/convictionSelect.js';
 import { OfficerSelect } from './officers/OfficerSelect.js';
 import { NoteForm } from '../notes/NoteForm.js';
+import { ShowNoteButton } from '../notes/ShowNotesButton.js';
+import '../notes/NoteList.js';
 
 CriminalList();
 ConvictionSelect(); 
 OfficerSelect();
 NoteForm();
+ShowNoteButton();
