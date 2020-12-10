7be903a5c0029a09a4b329a88869408f727f9107
MegSemrad <mlsemrad753@gmail.com>
2020-12-10 10:51:00 -0600

    worked on chapter 8

---
+++ scripts/alibis/associateAlibiDisplay.js
@@ -0,0 +1,25 @@
+import { getCriminals, useCriminals } from './criminalDataProvider.js';
+
+const contentTarget = document.querySelector(".associateAlibiContainer");
+const eventHub = document.querySelector(".container");
+
+export const associateAlibiSelect = () => {
+    getCriminals()
+    .then( () => {
+        const associatesAlibis = useCriminals()
+        render(associatesAlibis)
+    })
+};
+
+
+const render = associateAlibiCollection => {
+    return contentTarget.innerHTML = `
+        <section class="associate">
+            ${associateAlibiCollection.map((associate) => 
+                `<h2 class="assocaiteName">${associate.known_associates.name}</h2>
+                <div class="associateAlibi">Alibi: ${associate.known_associates.name}</div>`
+                )
+            }
+        </section>
+        `
+    };
