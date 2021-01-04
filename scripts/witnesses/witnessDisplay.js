7804a70be48e5a0f37dc386d281a1d8690b01b94
MegSemrad <mlsemrad753@gmail.com>
2021-01-04 07:56:23 -0600

    added witness statements button

---
+++ scripts/witnesses/witnessDisplay.js
@@ -0,0 +1,9 @@
+const eventHub = document.querySelector(".container");
+const contentTarget = document.querySelector("#criminalsContainer");
+
+
+
+/*
+- create button titled "Witness Statements in ".filters" element"
+
+*/
