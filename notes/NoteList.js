291dbaaca027a2ad0eaf9aa8eed7d249426a5848
MegSemrad <mlsemrad753@gmail.com>
2021-01-06 07:25:08 -0600

    changes

--- notes/NoteList.js
+++ notes/NoteList.js
@@ -1,5 +1,6 @@
 import { getNotes, useNotes } from "./NoteProvider.js";
 import { NoteHTMLConverter } from "./Note.js";
+import { useCriminals } from "../criminals/criminalDataProvider.js";
 
 // Query the DOM for the element that your notes will be added to 
 const contentTarget = document.querySelector(".noteList")
@@ -14,8 +15,17 @@ eventHub.addEventListener("noteStateChanged", () => {
     NoteList()
 })
 
-const render = (noteArray) => {
-    const allNotesConvertedToStrings = noteArray.map((note) => NoteHTMLConverter(note)).join("")
+const render = (noteArray, criminals) => {
+    const allNotesConvertedToStrings = noteArray.map((note) =>{
+      const associatedCriminal = criminals.find(
+        (criminal) => {
+          return criminal.id === note.criminal.id
+        }
+      )
+      note.criminalName = associatedCriminal.name
+      NoteHTMLConverter(note)
+}).join("")
+  
         /*
             - .map is expecting a function to be passed in as an arguement 
             - Side note: all array methods like .map() and .filter() and .find() are
@@ -34,10 +44,11 @@ const render = (noteArray) => {
 
 // Standard list function you're used to writing by now. BUT, don't call this in main.js! Why not?
 export const NoteList = () => {
+    let criminals = useCriminals()
     getNotes()
         .then(() => {
             const allNotes = useNotes()
-            render(allNotes)
+            render(allNotes, criminals)
         })
 }
 
