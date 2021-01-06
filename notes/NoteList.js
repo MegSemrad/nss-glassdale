1afb668d37b4e82be05340a3e4255645d45a4405
MegSemrad <mlsemrad753@gmail.com>
2021-01-06 10:37:12 -0600

    added delete button

--- notes/NoteList.js
+++ notes/NoteList.js
@@ -1,4 +1,4 @@
-import { getNotes, useNotes } from "./NoteProvider.js";
+import { getNotes, useNotes, deleteNote } from "./NoteProvider.js";
 import { NoteHTMLConverter } from "./Note.js";
 import { useCriminals } from "../scripts/criminals/criminalDataProvider.js"
 
@@ -13,6 +13,23 @@ eventHub.addEventListener("showNotesClicked", () => {
 
 eventHub.addEventListener("noteStateChanged", () => {
     NoteList()
+})
+
+eventHub.addEventListener("click", clickEvent => {
+  if (clickEvent.target.id.startsWith("deleteNote--")) {
+      const [prefix, noteId] = clickEvent.target.id.split("--")
+
+      // array destructoring 
+      // produces an array ["deleteNote--", "4"] 
+
+      /*
+          Invoke the function that performs the delete operation.
+
+          Once the operation is complete you should THEN invoke
+          useNotes() and render the note list again.
+      */
+     deleteNote(noteId)
+  }
 })
 
 const render = (noteArray, criminals) => {
